e2c9734de17a807c1dc9c4d4daa2a675827a0b8b
wardha <wardhakashif570@gmail.com>
2021-11-13 05:58:22 -0800

    copy and remove extra space

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function App() {
   return (
     <div>
       <Navbar title="TextUtils" aboutTextUtils="About TextUtils" />
-      {/* <TextForm heading="Enter Your Text Here"/> */}
-      <About />
+      <TextForm heading="Enter Your Text Here"/>
+      {/* <About /> */}
     </div>
   )
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -22,7 +22,21 @@ export default function TextForm(props) {
 
     const handleCamelCase = (e) => {
         let newtext = text.replace(/[-_\s.]+(.)?/g, (_, c) => c ? c.toUpperCase() : '');
-        setText(newtext .substr(0, 1).toLowerCase() + newtext.substr(1));
+        setText(newtext.substr(0, 1).toLowerCase() + newtext.substr(1));
+        e.preventDefault()
+    }
+    const handleCopy = (e) => {
+        console.log("I am copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+        e.preventDefault()
+    }
+
+    const handleExtraSpaces = (e) => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
         e.preventDefault()
     }
     return (
@@ -36,6 +50,8 @@ export default function TextForm(props) {
                     <button className="btn btn-primary mx-1" onClick={handleClick}>Capitalize</button>
                     <button className="btn btn-primary mx-1" onClick={handleClick1}>LowerCase</button>
                     <button className="btn btn-primary mx-1" onClick={handleCamelCase}>Camel Case</button>
+                    <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+                    <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
                     <div className="container my-3">
                         <p>The no of words are {text.split(' ').length} and {text.length} Characters.</p>
                         <p>{0.008 * text.split(" ").length} Minutes read</p>
